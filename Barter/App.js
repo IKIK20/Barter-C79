10c61a4077ca114f62fd625f7b175a39d2198d24
IKIK20 <67692637+IKIK20@users.noreply.github.com>
2021-06-23 11:17:12 +0530

    Add files via upload

---
+++ Barter/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import SignupLoginScreen from './screens/SignupLoginScreen';
+import HomeScreen from './screens/HomeScreen';
+import Exchange from './screens/Exchange';
+
+export default function App() {
+  return (
+    <SignupLoginScreen/>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
